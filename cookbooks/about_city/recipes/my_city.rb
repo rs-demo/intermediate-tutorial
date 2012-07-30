6d9a94cfedf843c44db0f386b6bb211997df6ca3
rs-demo <trainer@rightscale.com>
2012-07-30 23:54:59 +0000

    Added about_city cookbook

---
+++ cookbooks/about_city/recipes/my_city.rb
@@ -0,0 +1,11 @@
+rightscale_marker :begin
+
+  city_name = node[:about_city][:city_name]
+  temperature = node[:about_city][:temperature]
+  rain = node[:about_city][:rain]
+
+  log "I live in #{city_name}. The current temperature is #{temperature} degrees. And if you're wondering if you need an umbrella, the answer is #{rain}."
+            
+rightscale_marker :end
+
+
